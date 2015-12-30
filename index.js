4656ff417d16723940bf45dd4e76716f1ed7aa06
s9k <s9k0@yandex.ru>
2015-12-30 14:56:02 +0300

    Pass query options

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const glslify = require('glslify')
-const path    = require('path')
+const loaderUtils = require('loader-utils')
+const glslify     = require('glslify')
+const path        = require('path')
 
 module.exports = glslifyWebpackLoader
 
 function glslifyWebpackLoader(source) {
   var basedir = path.dirname(this.resourcePath)
+  var opts = loaderUtils.parseQuery(this.query)
   var self = this
+
+  opts.inline = true
+  opts.basedir = basedir
 
   this.callback = this.async()
   this.cacheable(true)
 
-  glslify.bundle(source, {
-    inline: true,
-    basedir: basedir
-  }, function(err, src, files) {
+  glslify.bundle(source, opts, function(err, src, files) {
     if (files) {
       files.forEach(function(file) {
         self.addDependency(file)
